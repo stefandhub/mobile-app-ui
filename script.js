2f5f60aad96204965bf2cda7f7a55696e37c2bd4
stefandhub <stefandifede@gmail.com>
2021-09-11 13:03:27 +0200

    [COMPLETED] Create page 1 general layout

---
+++ script.js
@@ -0,0 +1,12 @@
+const deviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        console.log("tablet");
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        return console.log("mobile");
+    }
+    console.log("desktop");
+};
+
+deviceType();       
