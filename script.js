4ba3011152977df40a4cb799755d0a8dc57a0fed
stefandhub <stefandifede@gmail.com>
2021-09-21 23:35:28 +0200

    [completed] reset filter button

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const deviceType = () => {
     const ua = navigator.userAgent;
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
         console.log("tablet");
-        alert("This is a mobile only app. Please use a mobile device to consult this app")
+        alert("This is a mobile-only app. Please use a mobile device to consult this app")
     }
     else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
         return console.log("mobile");
     }
     console.log("desktop");
-    alert("This is a mobile only app. Please use a mobile device to consult this app")
+    alert("This is a mobile-only app. Please use a mobile device to consult this app")
 };
 let page1 = document.getElementById("page__1");
 let page2 = document.getElementById("page__2");
@@ -23,26 +23,28 @@ class Doctor {
         this.info = info;
     }
 }
-
 const doctor1 = new Doctor("Dr. Stella Kane", "Heart Surgeon", "Flower Hospital", "Dr. Stella is the top most heart surgeon in Flower Hospital. She has done over 100 successful sugeries within past 3 years. She has achieved several awards for her wonderful contribution in her own field. She’s available for private consultation for given schedules.");
 const doctor2 = new Doctor("Dr. Joseph Cart", "Dental Surgeon", "Flower Hospital", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante eros, fringilla eget commodo nec, suscipit sit amet leo. Fusce mollis fringilla tellus, eget mollis urna volutpat nec. Integer consequat, tellus scelerisque efficitur accumsan, diam purus imperdiet sapien, ut cursus enim quam ut velit. Mauris quis nunc eget lacus ornare malesuada.");
 const doctor3 = new Doctor("Dr. Stefani Albert", "Heart Surgeon", "Flower Hospital","Nullam velit ipsum, tempor vel leo eget, eleifend rhoncus justo. Nullam cursus eu est eget scelerisque. Maecenas condimentum commodo nibh quis accumsan. Fusce non mauris non mauris volutpat congue. Donec interdum ipsum scelerisque felis eleifend dapibus.")
 
 const doctors = [doctor1, doctor2, doctor3];
+console.log(doctors);
 
 
 const backgroundColors = ["#EDF1FA", "#FAF2EA", "#F9EDEB"];
 
 function goToNextPage () {  
+    
     page1.style.display = "none";
     page2.style.display = "block";
+    
     //populating "top doctors" section
     for (let i=0; i < doctors.length; i++){
       let doctorBox = document.createElement("div");
       document.getElementById("page__2__top-doctors").appendChild(doctorBox);
       doctorBox.className = "doctor__box";
       doctorBox.setAttribute("id", "doctor"+[i])
-
+        
       let doctorName = document.createElement("div");
       doctorName.className= "doctor__name";
       doctorBox.appendChild(doctorName);
@@ -98,7 +100,6 @@ function doctorDetails (){
         doctorImage.src = "/images/doctor__3.png";
         doctorDescription.innerHTML = doctors[2].info;
     })
-
 }
 
 document.getElementById("page__3-back").addEventListener("click", backToPage2);
@@ -106,3 +107,93 @@ function backToPage2 (){
     page3.style.display = "none";
     page2.style.display = "block";
 }
+
+
+let dentalSurgeon = document.getElementById("dental");
+let heartSurgeon = document.getElementById ("heart");
+let eyeSurgeon = document.getElementById("eye");
+
+
+
+heartSurgeon.addEventListener("click" , heartFilter);
+dentalSurgeon.addEventListener("click", dentalFilter);
+eyeSurgeon.addEventListener("click", eyeFilter);
+/*function heart () {
+    for (let i = 0; i < Doctor[i]; i++){
+     Doctor[i].type = "Heart Surgeon";
+    }
+    console.log(filtered);
+    }*/
+
+/*function heartFilter ()  { 
+    let heartFilter = doctors.filter(function heart(el){
+    return el.type === "Heart Surgeon";
+    })
+    console.log(heartFilter); 
+
+} 
+
+function eyeFilter ()  {
+    let eyeFilter = doctors.filter(function eye(el){
+        return el.type === "Eye Surgeon";
+     })
+     console.log(eyeFilter);
+}
+
+function dentalFilter (){
+ let dentalFilter = doctors.filter(function dental(el){
+    return el.type === "Dental Surgeon";
+ })
+ console.log(dentalFilter)
+}*/
+let resetFilter = document.createElement("button");
+resetFilter.innerHTML = "Reset Filter";
+
+function heartFilter () {
+    doctorCard = document.getElementsByClassName("doctor__box");
+    for (i = 0; i < doctors.length; i++) {
+        if (doctors[i].type == "Heart Surgeon") {
+            doctorCard[i].style.display = ""
+        } else doctorCard[i].style.display = "none"
+    }
+    let topDoctors = document.getElementById("page__2__top-doctors");
+    topDoctors.insertAdjacentElement ("afterbegin",resetFilter);
+    resetFilter.addEventListener("click", reset)
+    resetFilter.style.display = "block";
+    resetFilter.className = "reset__button";
+}
+
+function dentalFilter () {
+    doctorCard = document.getElementsByClassName("doctor__box");
+    for (i = 0; i < doctors.length; i++) {
+        if (doctors[i].type == "Dental Surgeon") {
+            doctorCard[i].style.display = ""
+        } else doctorCard[i].style.display = "none"
+    }
+    let topDoctors = document.getElementById("page__2__top-doctors");
+    topDoctors.insertAdjacentElement ("afterbegin",resetFilter);
+    resetFilter.addEventListener("click", reset)
+    resetFilter.style.display = "block";
+    resetFilter.className = "reset__button";
+}
+
+function eyeFilter () {
+    doctorCard = document.getElementsByClassName("doctor__box");
+    for (i = 0; i < doctors.length; i++) {
+        if (doctors[i].type == "Eye Surgeon") {
+            doctorCard[i].style.display = ""
+        } else doctorCard[i].style.display = "none"
+    }
+    let topDoctors = document.getElementById("page__2__top-doctors");
+    topDoctors.insertAdjacentElement ("afterbegin",resetFilter);
+    resetFilter.addEventListener("click", reset)
+    resetFilter.style.display = "block";    
+    resetFilter.className = "reset__button";
+}
+
+function reset() {
+    doctorCard = document.getElementsByClassName("doctor__box");
+    for (i = 0; i < doctors.length; i++) {
+        doctorCard[i].style.display = ""
+        resetFilter.style.display = "none"
+    }}
